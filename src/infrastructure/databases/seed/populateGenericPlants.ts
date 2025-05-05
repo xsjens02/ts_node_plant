7eb0ce130a4968c18b2792d2e85a8116bbcd7e51
Simon <148754029+xsjens02@users.noreply.github.com>
2025-05-05 11:20:58 +0200

    fixed another error from db config

--- src/infrastructure/databases/seed/populateGenericPlants.ts
+++ src/infrastructure/databases/seed/populateGenericPlants.ts
@@ -5,12 +5,12 @@ import { GenericPlant } from "../../../domain/GenericPlant.js";
 import fs  from "fs/promises";
 
 export async function populateGenericPlants() {
-    const db = container.resolve<IPersistence<GenericPlant>>('GenericPlantMongo');
+    // const db = container.resolve<IPersistence<GenericPlant>>('GenericPlantMongo');
 
-    const filePath = path.resolve('./generic_plants.json');
-    const data = await fs.readFile(filePath, 'utf8');
-    const plants: GenericPlant[] = JSON.parse(data);
+    // const filePath = path.resolve('./generic_plants.json');
+    // const data = await fs.readFile(filePath, 'utf8');
+    // const plants: GenericPlant[] = JSON.parse(data);
 
-    for (const plant of plants) 
-        await db.create(plant);
+    // for (const plant of plants) 
+    //     await db.create(plant);
 } 
